2ed29a9d671d0764ad21ff72316525c361d54d14
anniverse02 <kukuladzeani02@gmail.com>
2026-07-24 12:00:44 +0400

    Add shared authentication functions

--- js/guard.js
+++ js/guard.js
@@ -1,3 +1,15 @@
+const logoutButton = document.getElementById("logoutButton");
+
+// Logout user
+function logout() {
+  localStorage.removeItem("crm_session");
+  window.location.href = "index.html";
+}
+
+if (logoutButton) {
+  logoutButton.addEventListener("click", logout);
+}
+
 // Protect pages that require login
 function checkAuth() {
   const savedSession = localStorage.getItem("crm_session");
@@ -7,6 +19,7 @@ function checkAuth() {
   }
 }
 
+// Redirect logged in users to dashboard
 function redirectIfLoggedIn() {
   const savedSession = localStorage.getItem("crm_session");
 
